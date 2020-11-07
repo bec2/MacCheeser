29fcaeb2b3d7ecf1c45192519cd90b596065a9fc
Beckett Hamilton <hamiltr2@aston.ac.uk>
2020-11-07 14:21:02 +0000

    Long live my obnoxious GUI work

--- main.py
+++ main.py
@@ -23,16 +23,13 @@ cv.create_image(0,0,image=bg_image,anchor='nw')
 mainframe=tk.Frame(root)
 
 #whats my mac
-def legacy():
-    print(gma())
-
 def macprinter():
     top=Toplevel(root)
     top.title=("Your MAC address is...")
     top.geometry("200x100")
     w2='200'
     h2='100'
-    cv2=tk.Canvas(master=top, width=w2,height=h2)
+    cv2=tk.Canvas(master=top, width=w2,height=h2,bg='#856ff9')
     cv2.pack(side='top',fill='y',expand='no')
     def getmymac():
         mactext=(gma())
@@ -44,8 +41,21 @@ def macprinter():
 
 #obfuscate me
 
-#restore 
+#restore
+
+#info
+def infostuff():
+    top2=Toplevel(root)
+    top2.title=("Info")
+    top2.geometry("500x500")
+    w3='500'
+    h3='500'
+    cv3=tk.Canvas(master=top2,width=w3,height=h3,bg='#f23a83')
+    cv3.pack(side='top',fill='y',expand='no')
+    cv3.create_text(15,20,text="What's A MAC Address?",fill="#FFFFFF",anchor='nw')
 
 #buttons
 btn1=tk.Button(cv,text="What's my MAC?",command=macprinter)
 btn1.pack(side='left',padx=10,pady=5,anchor='sw')
+btn2=tk.Button(cv,text="Info",command=infostuff)
+btn2.pack(side='left',padx=10,pady=5,anchor='sw')
